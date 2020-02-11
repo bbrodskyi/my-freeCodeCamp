f344b9d4e419a3b143f99ed397fe8b0d610bd559
Bohdan Brodskyi <brodskyy.b@outlook.com>
2020-02-11 16:20:18 +0100

    Completed challenges 1-5

---
+++ js-algorithms-and-data-structures-projects/basic-algorithm-scripting/challenge-3.js
@@ -0,0 +1,10 @@
+// Hint link: https://www.freecodecamp.org/forum/t/freecodecamp-challenge-guide-factorialize-a-number/16013
+
+function factorialize(num) {
+    for (var product = 1; num > 0; num--) {
+        product *= num;
+    }
+    return product;
+}
+  
+console.log(factorialize(5));
