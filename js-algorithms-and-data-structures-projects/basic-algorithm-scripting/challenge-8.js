8f4debfc655f26c39b9a5c0b1d6985e14cf692eb
Bohdan Brodskyi <brodskyy.b@outlook.com>
2020-02-11 20:42:15 +0100

    completed challenges 6 to 12

---
+++ js-algorithms-and-data-structures-projects/basic-algorithm-scripting/challenge-8.js
@@ -0,0 +1,18 @@
+// Hint link: https://www.freecodecamp.org/forum/t/freecodecamp-challenge-guide-truncate-a-string/16089
+
+function truncateString(str, num) {
+    // Clear out that junk in your trunk
+    
+    if (str.length > num) {
+      let result = "";
+      for (let i = 0; i < num; i++) {
+        result += str[i];
+      }
+      result += "...";
+      return result;
+    }
+    return str;
+}
+  
+truncateString("A-tisket a-tasket A green and yellow basket", 8);
+  
